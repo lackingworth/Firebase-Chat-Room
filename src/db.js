b152dd1d7c5a1592dc557c18b1c2a6babe68d46b
lackingworth <worstface@yandex.ru>
2023-07-16 13:26:14 +0300

    initial commit

---
+++ src/db.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
+
+const config = {
+    //API KEYS
+}
+
+const firebaseApp = firebase.initializeApp(config);
+
+export default firebaseApp;
